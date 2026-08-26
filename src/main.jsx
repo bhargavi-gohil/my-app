42f34c4903fc85c46763d768c8cd7a96b911f862
Bhargavi Gohil <bhargavigohil098@gmail.com>
2026-08-26 10:58:09 +0530

    fix: reorganize project structure - move all files to src/ folder

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,13 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 
-function App() {
-  return (
-    <div>
-      <h1>React Jobs</h1>
-      <p>Entry point added to fix Vite build error.</p>
-    </div>
-  );
-}
-
-createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <ToastContainer />
   </React.StrictMode>
 );
